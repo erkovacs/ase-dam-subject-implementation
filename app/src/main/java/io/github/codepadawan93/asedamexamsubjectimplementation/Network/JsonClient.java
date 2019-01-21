c529437ae03f1110a7153437d2c5f2744672a00c
Kovacs Erik <kovaroberik@gmail.com>
2019-01-21 22:42:11 +0200

    before merge

---
+++ app/src/main/java/io/github/codepadawan93/asedamexamsubjectimplementation/Network/JsonClient.java
@@ -0,0 +1,26 @@
+package io.github.codepadawan93.asedamexamsubjectimplementation.Network;
+
+import android.os.AsyncTask;
+
+public class JsonClient extends AsyncTask<String, String, String> {
+
+    @Override
+    protected String doInBackground(String... strings) {
+        return null;
+    }
+
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+    }
+
+    @Override
+    protected void onPostExecute(String s) {
+        super.onPostExecute(s);
+    }
+
+    @Override
+    protected void onProgressUpdate(String... values) {
+        super.onProgressUpdate(values);
+    }
+}
